f168ee27f55c625c4cdea1efdf190f16e4ffebd4
Igor <ife.developer.company@gmail.com>
2021-07-10 16:00:05 +0400

    create new project

---
+++ ServerSpring/src/test/java/com/ife/develeper/server/ServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ife.develeper.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
